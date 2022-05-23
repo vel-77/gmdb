6e3529d46af5bbda126ee2c93af1aea1f8952c6e
Vardanyan <velison.vardanyan@teamsolutions.am>
2022-05-23 22:47:33 +0400

    initial commit

---
+++ src/main/java/com/gmdb/gmdb/GmdbApplication.java
@@ -0,0 +1,13 @@
+package com.gmdb.gmdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmdbApplication.class, args);
+    }
+
+}
